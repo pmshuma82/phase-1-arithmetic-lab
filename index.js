a7c88a9d909165450db4e7642af0bfed7fe9f05d
Paul M Shumate <+>
2024-02-02 16:05:53 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // Result should be 62
 
-// Write your code here
+// Challenge 2: Create a variable called random
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1; // Generates a random integer greater than 0
+
+// Challenge 3: Create a variable called mod
+const num3 = 40;
+const num4 = 9;
+const mod = num3 % num4; // Remainder should be 4
+
+// Challenge 4: Create a variable called max
+const max = Math.max(2, 4, 6, 8, 10, 12, 14, 16, 18, 20); // Highest number should be 20
